9ecc869ae443e0d3812db1dc92a333afe05fac95
Bogdan Oporanu <bogdan.oporanu@aurachain.ch>
2021-04-15 14:35:52 +0300

    Second iteration - added bezier for branches (curve based on left/right position) - added arc for leaves and different color - added random angles so the tree isn't symmetric (as suggested)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,24 +20,37 @@ export default {
         this.draw(canvas.width / 2, canvas.height, 0, 20, 90);
     },
     methods: {
-        draw(x, y, angle, width, size, color1 = "#0F0") {
+        draw(x, y, angle, width, size, color1 = "#0F0", color2 = "#F00") {
             ctx.beginPath();
             ctx.save();
             ctx.strokeStyle = color1;
+            ctx.fillStyle = color2;
             ctx.lineWidth = width;
-            ctx.translate(x, y);
+            ctx.translate(x, y + 5); // added +5 so that the branches will be more compact
             ctx.moveTo(0, 0);
             ctx.rotate((angle * Math.PI) / 180);
-            ctx.lineTo(0, -size);
+            if (angle > 0) {
+                // right branch
+                ctx.bezierCurveTo(5, -size / 2, 10, -size / 2, 0, -size);
+            } else {
+                // left branch
+                ctx.bezierCurveTo(5, -size / 2, -10, -size / 2, 0, -size);
+            }
             ctx.stroke();
 
             if (size < 9) {
+                // last draw will be the leaf
+                ctx.beginPath();
+                ctx.arc(0, -size, 10, 0, Math.PI / 2.2);
+                ctx.fill();
                 ctx.restore();
                 return;
             }
 
-            this.draw(0, -size, angle + 8, width * 0.7, size * 0.82);
-            this.draw(0, -size, angle - 8, width * 0.7, size * 0.82);
+            // some randomness to the branch curve as suggested
+            const curve = (Math.random() * 10) + 10;
+            this.draw(0, -size, angle + curve, width * 0.7, size * 0.82);
+            this.draw(0, -size, angle - curve, width * 0.7, size * 0.82);
             ctx.restore();
         }
     }
